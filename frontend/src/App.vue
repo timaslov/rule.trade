52a0cc85dc89ba0d7fbe8f78b84c67a85cd3bfbd
timaslov <timaslov@yandex.ru>
2023-10-02 20:11:36 +0300

    Создал проект с vite, vuex, ts, vue-router, eslint, prettier. Сделал верстку страницы HomePage по макету.

---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HomePage from './views/HomePage.vue';
+</script>
+
+<template>
+  <HomePage/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/HomePage.vue
@@ -0,0 +1,373 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="background">
+    <div class="introd_block">
+      <h1>Автоматизированная<br> торговля по Вашим правилам</h1>
+
+      <p>Дайте себе свободу от рутины торговли на бирже. Rule.trade - это инновационный сервис,
+        позволяющий Вам создавать и применять торговые стратегии автоматически, даже без навыков
+        программирования. Просто подключите свой аккаунт на бирже, настройте логические правила
+        на основе выбраных технических индикаторов, и начните торговать на автопилоте.</p>
+
+      <button class="introd_button">Начать прямо сейчас</button>
+
+      <img
+          src="../assets/images/HomePage_introd_block_image.jpg"
+          class="introd_img"
+          alt="Some_text"
+      >
+    </div>
+  </div>
+
+  <div class="about_us_block">
+    <h2>Наши особенности:</h2>
+
+    <p>С rule.trade Вы получаете мощный инструмент для автоматизации Вашей торговли на бирже. Наш сервис
+      разработан для удовлетворения потребностей трейдеров всех уровней опыта. Вот почему наши особенности
+      выгодны для Вас:</p>
+
+    <div class="advantages">
+      <div class="advantage_item">
+        <img src="../assets/images/HomePage_about_us_icon1.png" alt="Icon1" />
+        <h3>Простота</h3>
+        <ul class="u_list">
+          <li class="list_item">Теперь для автоматизации торговли не требуется писать код
+            на языках программирования или постоянно следить за рынком</li>
+          <li class="list_item">Работает 24/7. Настройте правила и занимайтесь своими делами,
+            rule.trade совершает сделки всегда</li>
+        </ul>
+      </div>
+      <div class="advantage_item">
+        <img src="../assets/images/HomePage_about_us_icon2.png" alt="Icon2" />
+        <h3>Безопасность</h3>
+        <ul class="u_list">
+          <li class="list_item">Все ваши средства хранятся на подключённых биржах,
+            rule.trade не имеет к ним доступ</li>
+          <li class="list_item">Настройте доступ к вашим API только по определённым IP,
+            злоумышленник даже узнав ваш ключ не сможет совершить действия</li>
+        </ul>
+      </div>
+      <div class="advantage_item">
+        <img src="../assets/images/HomePage_about_us_icon3.png" alt="Icon3" />
+        <h3>Для всех уровней опыта</h3>
+        <ul class="u_list">
+          <li class="list_item">Подходит для любого опыта торговли - просто перенесите
+            опубликованные в Интернете стратегии, или создайте свою</li>
+          <li class="list_item">Если ваше настроенное правило не будет показывать
+            необходимую эффективность - просто остановите его или заново запустите одним кликом</li>
+        </ul>
+      </div>
+    </div>
+
+    <button class="introd_button">Попробовать</button>
+  </div>
+
+  <div class="exchanges_block">
+    <h2>Доступные биржи</h2>
+
+    <p>Список бирж, доступных к подключению, постоянно расширяется</p>
+
+    <div class="exchanges_flexbox">
+      <div class="exchanges_flexbox_item">
+        <img src="../assets/images/HomePage_about_us_icon1.png" alt="Icon3" />
+      </div>
+      <div class="exchanges_flexbox_item">
+        <img src="../assets/images/HomePage_about_us_icon1.png" alt="Icon3" />
+      </div>
+      <div class="exchanges_flexbox_item">
+        <img src="../assets/images/HomePage_about_us_icon1.png" alt="Icon3" />
+      </div>
+      <div class="exchanges_flexbox_item">
+        <img src="../assets/images/HomePage_about_us_icon1.png" alt="Icon3" />
+      </div>
+    </div>
+  </div>
+
+  <div class="questions_block">
+    <div class="questions_block_top">
+      <div class="question_mark">Вопросы</div>
+
+      <h2>Как начать работу с нашим сервисом?</h2>
+
+      <p>Мы готовы ответить на самые популярные вопросы, чтобы обеспечить вас
+        всей необходимой информацией о нашем сервисе.</p>
+    </div>
+
+    <div class="questions_flexbox">
+      <div class="questions_flexbox_item">
+        <div class="question_icon"><span>?</span></div>
+        <div class="questions_flexbox_item_item">
+          <h3>Что такое правило?</h3>
+          <p>Правило - заданные вами действия, которые будут выполнены в случае
+            выполнения указанных условий по необходимым техническим индикаторам</p>
+        </div>
+      </div>
+      <div class="questions_flexbox_item">
+        <div class="question_icon"><span>?</span></div>
+        <div class="questions_flexbox_item_item">
+          <h3>Что необходимо для настройки правила?</h3>
+          <p>Вам необходимо иметь верифицированный аккаунт на любой из доступных бирж,
+            аккаунт в сервисе TradingView и наличие подписки в rule.trade</p>
+        </div>
+      </div>
+      <div class="questions_flexbox_item">
+        <div class="question_icon"><span>?</span></div>
+        <div class="questions_flexbox_item_item">
+          <h3>Что такое пакет?</h3>
+          <p>Пакет - набор технических индикаторов, которые добавлены в ваш чарт на TradingView</p>
+        </div>
+      </div>
+      <div class="questions_flexbox_item">
+        <div class="question_icon"><span>?</span></div>
+        <div class="questions_flexbox_item_item">
+          <h3>Что такое логика в rule.trade ?</h3>
+          <p>Нет, сервис создан для того, чтобы обеспечить простое и интуитивное
+            создание стратегий, даже без программирования.</p>
+        </div>
+      </div>
+      <div class="questions_flexbox_item">
+        <div class="question_icon"><span>?</span></div>
+        <div class="questions_flexbox_item_item">
+          <h3>Как я могу анализировать результаты моих стратегий?</h3>
+          <p>Вам доступен инструмент для анализа доходности ваших стратегий,
+            который поможет вам оптимизировать результаты.</p>
+        </div>
+      </div>
+      <div class="questions_flexbox_item">
+        <div class="question_icon"><span>?</span></div>
+        <div class="questions_flexbox_item_item">
+          <h3>Каким образом я могу начать использовать сервис?</h3>
+          <p>Процесс начала использования подробно описан в разделе "Инструкция по настройке" на нашем сайте.</p>
+        </div>
+      </div>
+      <div class="didnt_find_answer">
+        <h2>Попробуйте сейчас создать торговое правило</h2>
+        <p>Мы предоставляем надежный и удобный доступ к глобальным финансовым рынкам, позволяя вам
+          максимально эффективно управлять вашими инвестициями. Начните с нами свой финансовый успех уже сегодня!</p>
+        <button>Попробовать сейчас</button>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+.background {
+  position: relative;
+}
+
+.background::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 100vw;
+  height: 100%;
+  background-image: url('../assets/images/HomePage_introd_block_background.jpg');
+  background-size: cover;
+  background-position: center bottom;
+  background-repeat: no-repeat;
+  filter: blur(10px);
+  z-index: -1;
+}
+
+.introd_block {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  max-width: 968px;
+  margin: 0 auto;
+}
+
+.introd_button {
+  margin-top: 2em;
+}
+
+.introd_img {
+  max-width: 90%;
+  height: auto;
+  margin-top: 36px;
+  margin-bottom: 4em;
+  border-radius: 30px;
+}
+
+.about_us_block {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 64px;
+}
+
+.about_us_block p {
+  color: #8896AB;
+}
+
+.advantages {
+  display: flex;
+  width: 100%;
+  margin-top: 24px;
+}
+
+.advantage_item {
+  flex: 1;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  background-color: #ffffff;
+  border-radius: 16px;
+  box-shadow: 0 2px 10px 0 #d9d9d9;
+  margin-right: 32px;
+}
+
+.advantage_item:last-child {
+  margin-right: 0; /* убирает промежуток у последнего прямоугольника */
+}
+
+.u_list {
+  margin: 0 0 0 0;
+  padding: 0 0 0 48px;
+}
+
+.list_item {
+  font-size: 20px;
+  line-height: 30px;
+  text-align: left;
+  color: #556987
+}
+
+.exchanges_block {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 64px;
+}
+
+.exchanges_block p{
+  color: #8896AB;
+}
+
+.exchanges_flexbox {
+  display: flex;
+  width: 100%;
+  margin-top: 32px;
+}
+
+.exchanges_flexbox_item {
+  flex: 1;
+  background-color: #ffffff;
+  border-radius: 16px;
+  box-shadow: 0 2px 10px 0 #d9d9d9;
+  margin-right: 32px;
+  padding: 42px;
+}
+
+.questions_block {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 64px;
+}
+
+.questions_block_top {
+  margin-top: 32px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  text-align: left;
+}
+
+.questions_block_top h2 {
+  margin: 8px 0 16px 0;
+}
+
+.questions_block_top p {
+  color: #8896AB;
+}
+
+.question_mark {
+  color: #007EC6;
+  background-color: #E2E8F0;
+  padding: 8px 16px 8px 16px;
+  border-radius: 36px;
+  font-size: 20px;
+  line-height: 18px;
+}
+
+.questions_flexbox {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin-top: 32px;
+}
+
+.questions_flexbox_item {
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  background-color: #ffffff;
+  border-radius: 16px;
+  box-shadow: 0 2px 10px 0 #d9d9d9;
+  padding: 16px;
+  margin: 0 0 32px 0;
+}
+
+.questions_flexbox_item_item {
+  text-align: left;
+}
+
+.questions_flexbox_item_item h3{
+  margin: 0 0 24px 0;
+}
+
+.questions_flexbox_item_item p{
+  color: #8896AB;
+}
+
+.question_icon {
+  background-color: #007EC6;
+  min-width: 48px;
+  width: 48px;
+  height: 48px;
+  border-radius: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #ffffff;
+  font-size: 24px;
+  margin: 0 12px 0 12px;
+}
+
+.didnt_find_answer {
+  background-color: #ffffff;
+  border-radius: 16px;
+  box-shadow: 0 2px 10px 0 #d9d9d9;
+  padding: 32px;
+  margin: 0 0 32px 0;
+}
+
+.didnt_find_answer h2 {
+  margin: 0 0 0 0;
+}
+
+.didnt_find_answer p {
+  color: #8896AB;
+}
+
+.didnt_find_answer button {
+  margin: 24px 0 0 0;
+}
+
+</style>
