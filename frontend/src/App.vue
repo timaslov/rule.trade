d1857fafcee1a10d1a0adc585c1ef1dd4ce987b5
timaslov <timaslov@yandex.ru>
2023-11-03 21:57:18 +0300

    Добавил спиннер

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,14 @@
 <script setup lang="ts">
 import NavBar from "./components/NavBar.vue";
 import AppFooter from "./components/AppFooter.vue";
+import SpinnerComponent from "./components/SpinnerComponent.vue";
+
+import {useStore} from "vuex";
+import {computed} from "vue";
+
+const store = useStore();
+const isSpinnerShown = computed(() => store.state.spinner.isVisible);
+
 </script>
 
 <template>
@@ -8,6 +16,7 @@ import AppFooter from "./components/AppFooter.vue";
 
   <main class="main">
     <RouterView/>
+    <spinner-component v-if="isSpinnerShown"/>
   </main>
 
     <AppFooter/>
---
+++ frontend/src/components/SpinnerComponent.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="loader-container">
+    <span class="loader"></span>
+  </div>
+</template>
+
+<style scoped>
+.loader-container{
+  position:fixed;
+  top:50%;
+  left:50%;
+  transform: translate(-50%, -50%);
+}
+.loader {
+  width: 64px;
+  height: 64px;
+  border: 10px solid black;
+  border-radius: 50%;
+  display: inline-block;
+  position: relative;
+  box-sizing: border-box;
+  animation: rotation 1s linear infinite;
+}
+.loader::after,
+.loader::before {
+  content: '';
+  box-sizing: border-box;
+  position: absolute;
+  background: black;
+  width: 12px;
+  height: 12px;
+  border-radius: 50%;
+}
+.loader::before {
+  right: -13px;
+  bottom: -13px;
+}
+
+.loader::after {
+  left: -13px;
+  top: -13px;
+}
+
+@keyframes rotation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
